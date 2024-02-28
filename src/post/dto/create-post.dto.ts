ad2333ce730a74ed3702bd9493dab70125fe97e2
longnt <longn7284@gmail.com>
2024-02-28 10:26:41 +0700

    update doc

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -3,13 +3,14 @@ import {
   CONTENT_MAX_LEN,
   CONTENT_MIN_LEN,
   ICreatePost,
+  TITLE_MAX_LEN,
   TITLE_MIN_LEN,
 } from '../post.interface';
 
 export interface CreatePostDto extends Omit<ICreatePost, 'views' | 'user'> {
   title: string &
     tags.MinLength<typeof TITLE_MIN_LEN> &
-    tags.MaxLength<typeof TITLE_MIN_LEN>;
+    tags.MaxLength<typeof TITLE_MAX_LEN>;
 
   content: string &
     tags.MinLength<typeof CONTENT_MIN_LEN> &
